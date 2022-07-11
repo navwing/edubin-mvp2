3bc3049c914f69ef34f3c1d71b055c1a708d2b68
navwing <106735381+navwing@users.noreply.github.com>
2022-07-11 17:17:21 +0700

    fix

--- js/register.js
+++ js/register.js
@@ -1,3 +1,12 @@
 function validateForm() {
-   
+    var name=Document.myForm.name.value;  
+    var password=Document.myForm.password.value;  
+      
+    if (name==null || name==""){  
+      alert("Name can't be blank");  
+      return false;  
+    }else if(password.length<6){  
+      alert("Password must be at least 6 characters long.");  
+      return false;  
+      }  
   }
